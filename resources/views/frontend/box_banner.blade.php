74cad914bb85398f50e0a1a92d417b7a4faea55b
Quan Do <do.manh.quan@vsi-international.com>
2015-11-06 21:12:42 +0700

    fix

--- resources/views/frontend/box_banner.blade.php
+++ resources/views/frontend/box_banner.blade.php
@@ -4,17 +4,17 @@
         <div class="owl-carousel" id="slideHomepage">
             <div class="item">
                 <a class="thumb" href="" title="">
-                    <img src="imgs/temp/Banner-03-01.jpg"/>
+                    <img src="{{url('imgs/temp/Banner-03-01.jpg')}}"/>
                 </a>
             </div>
             <div class="item">
                 <a class="thumb" href="" title="">
-                    <img src="imgs/temp/Banner-03-02.jpg"/>
+                    <img src="{{url('imgs/temp/Banner-03-02.jpg')}}"/>
                 </a>
             </div>
             <div class="item">
                 <a class="thumb" href="" title="">
-                    <img src="imgs/temp/Banner-03-03.jpg"/>
+                    <img src="{{url('imgs/temp/Banner-03-03.jpg')}}"/>
                 </a>
             </div>
         </div>
